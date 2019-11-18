0fc3bb8a33e27dd5ec29e978786358c59b412998
Beniseeger <beniseeger@web.de>
2019-11-18 13:35:14 +0100

    Added Presentation and Paymentchannel test

--- app/src/app/payment-channel.service.ts
+++ app/src/app/payment-channel.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class PaymentChannelService {
 
   constructor() { }
+
+  initializePaymentChannels(senderAccount: any, receiverAccount: any, nonce: any) { 
+    //const amount = parseInt((<HTMLInputElement>document.getElementById("amount")).value);
+    const { claimPayment } = this.fairChargerContract.methods;
+    await claimPayment(receiverAccount, amount, nonce).send({ from: this.account });
+
+    this.setStatus("Payment sent!");
+    this.refreshBalance();
+  }
 }

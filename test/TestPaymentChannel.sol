f6f4db770c71ab0b3ded4fb9903a001afb8d28f4
Beniseeger <beniseeger@web.de>
2019-11-25 16:16:56 +0100

    Removed unneccessary test and varaibles

--- test/TestPaymentChannel.sol
+++ test/TestPaymentChannel.sol
@@ -6,9 +6,6 @@ import "../contracts/FairCharger.sol";
 
 contract TestPaymentChannel {
 
-  mapping (address => uint256) private _balances;
-  mapping (address => mapping (address => uint256)) private _allowances;
-
   function testInitialBalanceUsingDeployedContract() public{
     FairCharger charger = FairCharger(DeployedAddresses.FairCharger());
 
@@ -37,19 +34,6 @@ contract TestPaymentChannel {
     Assert.equal(charger.decimals(), expected, "Cut 2 decimals");
   }
 
-  function testPaymentChannelDuration() public {
-   /* FairCharger charger = FairCharger(DeployedAddresses.FairCharger());
-
-    address payable recipient = 0x820Fc7728d51992421E096317849C4d9b143ae8B;
-
-    uint duration = 10;
-
-    uint expectedDuration = block.timestamp + duration;*/
-
-   // charger.SimplePaymentChannel(recipient,duration);
-    //Assert.equal(expiration, expectedDuration, "Arsch mit Soß");
-  }
-
   function testSymbol() public {
     FairCharger charger = FairCharger(DeployedAddresses.FairCharger());
     string memory expected = "Fair";
@@ -59,10 +43,8 @@ contract TestPaymentChannel {
   function testTransferSender() public {
     FairCharger charger = FairCharger(DeployedAddresses.FairCharger());
     uint256 amount = 1000;
-    _balances[msg.sender] = charger.balanceOf(msg.sender);
-    uint256 expected = _balances[msg.sender] - amount;
     charger.transfer(amount);
-    Assert.equal(charger.balanceOf(msg.sender), expected, "Sender received the Payment of 10.");
+    Assert.equal(charger.balanceOf(msg.sender), expected, "Sender didnt received the Payment of 10.");
   }
 
 }

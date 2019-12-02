429caf293a081ff9227f82491d8efe8d891dd68f
NicoVogel <nico-vogel97@outlook.de>
2019-12-02 21:22:51 +0000

    add prefixed in solidity

--- contracts/FairCharger.sol
+++ contracts/FairCharger.sol
@@ -80,4 +80,10 @@ contract FairCharger {
 
         return ecrecover(message, v, r, s);
     }
+    
+    // Builds a prefixed hash to mimic the behavior of eth_sign.
+    function prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256("\x19Ethereum Signed Message:\n32", hash);
+    }
+    
 }

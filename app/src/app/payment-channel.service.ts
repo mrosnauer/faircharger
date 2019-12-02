9d56dc2a228a2c96376ea92d4e9c95ba996a5c17
mrosnauer <mrosnauer@gmail.com>
2019-12-02 23:41:00 +0100

    Added comments

--- app/src/app/payment-channel.service.ts
+++ app/src/app/payment-channel.service.ts
@@ -20,87 +20,39 @@ export class PaymentChannelService {
   contractAddress;
   private web3;
 
+  /**
+   * Setup Payment channel
+   * @param web3 web3 to be injected
+   * @param driverAccount the account of the driver
+   * @param chargeStickAccount the account of the charge stick owner
+   * @param maxVal the maximum value. The Contract will be funded with this amount
+   * @param callback callback function
+   */
   async init(web3: Web3, driverAccount, chargeStickAccount, maxVal, callback) {
     this.web3 = web3;
     let contracts: any = fairCharger;
     let abi = contracts.default.abi;
     let contract = new web3.eth.Contract(abi, null, { gas: 1000000 });
-    //contract.transactionConfirmationBlocks = 1;
     let code = contracts.default.bytecode;
 
-
+    //Estimate Gas cost
     const gas = await contract.deploy({ data: code, arguments: [chargeStickAccount, 100] }).estimateGas();
+    //Deploy contract
     const obj = await contract.deploy({ data: code, arguments: [chargeStickAccount, 100] });
+    //Send contract
     const obj2 = await obj.send({
       from: driverAccount,
       gas: gas + 1,
       value: maxVal
     }, () => callback());
-    console.log("TE213123ST");
+    //On our machine, web3 was not passing the await ans infinitely stuck. See Github link
     return obj2;
-
-
-    /*contract.deploy({
-      data: code
-    }).send({
-      from: '0xc63a3709bD763ea9875505EBBA761E85FCE13f19',
-      gas: 1500000,
-      gasPrice: '30000000000000',
-      value: maxVal
-    }, (error, transactionHash) => {
-      console.log("Error");
-    }).on('error', function(error){ 
-      console.log("ERROR");
-    }).on('transactionHash', function(transactionHash){
-      console.log("ERROR2");
-     })
-    .on('receipt', function(receipt){
-       console.log(receipt.contractAddress) // contains the new contract address
-    })
-    .on('confirmation', function(confirmationNumber, receipt) {
-      console.log("TEST345");
-    })
-    .then(function(newContractInstance){
-        console.log(newContractInstance.options.address) // instance with the new contract address
-    });*/
-
-
-    /*.then(function (newContractInstance) {
-      console.log(newContractInstance.options.address); // instance with the new contract address
-      this.contractAddress = newContractInstance.options.address;
-    }).catch((error) => {
-      console.log(error);
-    });*/
-
   }
-
-  initializePaymentChannels(senderAccount: any, receiverAccount: any, nonce: any) {
-    //const amount = parseInt((<HTMLInputElement>document.getElementById("amount")).value);
-    /*const { claimPayment } = this.fairChargerContract.methods;
-    await claimPayment(receiverAccount, amount, nonce).send({ from: this.account });
-
-    this.setStatus("Payment sent!");
-    this.refreshBalance();*/
-  }
-
-  private constructPaymentMessage(amount) {
-    /*return ethereumjs.soliditySHA3(
-      ["address", "uint256"],
-      [this.contractAddress, amount],
-    );*/
-    return ethereumjs.soliditySHA3(["address", "uint"], [new BN(this.contractAddress.replace('x', ''), 16), amount]).toString('hex');
-    return ethereumjs.soliditySHA3(
-      ["address", "address", "uint", "uint"],
-      [new BN("03d3ef18442361D220Cb14313D7B6e142dA276Ab", 16), 0, 1000, 1448075779]
-    ).toString('hex');
-
-  }
-
 
   // contractAddress is used to prevent cross-contract replay attacks.
   // amount, in wei, specifies how much ether should be sent.
   signPayment(amount, callback) {
-    var message = this.constructPaymentMessage(amount);
+    var message = ethereumjs.soliditySHA3(["address", "uint"], [new BN(this.contractAddress.replace('x', ''), 16), amount]).toString('hex');
     this.web3.eth.getAccounts((error, accounts) => {
       this.web3.eth.personal.sign("0x" + message.toString("hex"), accounts[0],
         callback);

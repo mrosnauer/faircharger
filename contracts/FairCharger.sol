5f756d863fd8017856768a87dadfe0b41e4840d1
NicoVogel <nico-vogel97@outlook.de>
2019-12-02 19:55:08 +0000

    update

--- contracts/FairCharger.sol
+++ contracts/FairCharger.sol
@@ -51,23 +51,20 @@ contract FairCharger {
         selfdestruct(sender);
     }
 
+   /// signature methods.
     function splitSignature(bytes memory sig)
         internal
         pure
-        returns (uint8, bytes32, bytes32)
+        returns (uint8 v, bytes32 r, bytes32 s)
     {
         require(sig.length == 65);
 
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
         assembly {
-            // first 32 bytes, after the length prefix
+            // first 32 bytes, after the length prefix.
             r := mload(add(sig, 32))
-            // second 32 bytes
+            // second 32 bytes.
             s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
+            // final byte (first byte of the next 32 bytes).
             v := byte(0, mload(add(sig, 96)))
         }
 
@@ -79,29 +76,8 @@ contract FairCharger {
         pure
         returns (address)
     {
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-
-        (v, r, s) = splitSignature(sig);
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
 
         return ecrecover(message, v, r, s);
-    }
-
-    // Builds a prefixed hash to mimic the behavior of eth_sign.
-    function prefixed(bytes32 hash) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
-    }
-
-    function claimPayment(uint256 amount, uint256 nonce, bytes sig) public {
-        require(!usedNonces[nonce]);
-        usedNonces[nonce] = true;
-
-        // This recreates the message that was signed on the client.
-        bytes32 message = prefixed(keccak256(msg.sender, amount, nonce, this));
-
-        require(recoverSigner(message, sig) == owner);
-
-        msg.sender.transfer(amount);
     }
 }
